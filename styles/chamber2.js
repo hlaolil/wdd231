c58446bb0066ec3253e3aa5106f193ce2db745a4
hlaolil <167699704+hlaolil@users.noreply.github.com>
2025-01-13 19:42:44 +0200

    Update chamber2.js

--- styles/chamber2.js
+++ styles/chamber2.js
@@ -1,1 +1,6 @@
-
+courses.forEach(course => {
+  const courseCard = document.createElement('div'); // Create a child div for each course
+  courseCard.className = 'course-grid-item'; // Add styling
+  courseCard.textContent = course; // Set the content (course name)
+  coursesDiv.appendChild(courseCard); // Append it to the container
+});
