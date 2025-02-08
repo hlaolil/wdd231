f2335773fa948c0635341c1db7c3ff8006601c9d
hlaolil <hla24002@byui.edu>
2025-02-08 16:16:23 +0200

    w5

--- chamber/scripts/discover.js
+++ chamber/scripts/discover.js
@@ -1,3 +1,47 @@
+
+const hamButton = document.querySelector('.ham-button');
+const navigation = document.querySelector('.navigation');
+
+// Initial HTML content for the hamburger icon
+const hamburgerHTML = `
+            <span class="bar"></span>
+            <span class="bar"></span>
+            <span class="bar"></span>
+        `;
+
+// Close icon HTML
+const closeIconHTML = '✖';
+
+// Track the state of the menu
+let isMenuOpen = false;
+
+hamButton.addEventListener('click', () => {
+    isMenuOpen = !isMenuOpen; // Toggle the menu state
+
+     if (isMenuOpen) {
+        hamButton.innerHTML = closeIconHTML; // Change to close icon
+        navigation.classList.add('show'); // Show the navigation
+        hamButton.setAttribute('aria-expanded', 'true'); // Update ARIA attribute
+    } else {
+        hamButton.innerHTML = hamburgerHTML; // Change back to hamburger icon
+        navigation.classList.remove('show'); // Hide the navigation
+        hamButton.setAttribute('aria-expanded', 'false'); // Update ARIA attribute
+    }
+});
+
+
+const year = document.querySelector("#currentyear");
+const lastModified = document.querySelector("#lastModified");
+
+// Get the current year for footer
+const date = new Date();
+year.textContent = date.getFullYear();
+
+// Get the last modification date of the home page
+let lastMod = document.lastModified;
+lastModified.textContent = `Last Modified: ${lastMod}`;
+
+
 // Store the current visit date
 const currentVisitDate = new Date().toLocaleString();
 
