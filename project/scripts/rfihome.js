1d9ea2c350ae5d82ffce6ddc32c739eef16437e5
hlaolil <hla24002@byui.edu>
2025-02-15 15:41:56 +0200

    w6

--- project/scripts/rfihome.js
+++ project/scripts/rfihome.js
@@ -215,24 +215,7 @@ function displaySpotlight() {
                 image: "sunday.jpg",
                 contact: "Church Office - +266 6223 3969"
             },
-            { 
-                name: "Youth Meeting", 
-                schedule: "Every Saturday, 15:00",
-                image: "youth.jpg",
-                contact: "Youth Leader - +266 5912 6226"
-            },
-            { 
-                name: "Women’s Meeting", 
-                schedule: "Every Thursday, 14:00",
-                image: "women.jpg",
-                contact: "Women’s Ministry - +266 63135521"
-            },
-            {
-                name: "Children's Ministry",
-                schedule: "Every Sunday, 1030 - 1130",
-                image: "children.jpg",
-                contact: "Teacher - +266 5919 3208"
-            }
+            
         ];
 
         // Use a DocumentFragment for performance
